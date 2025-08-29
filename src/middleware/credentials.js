b0358e7406f5b7b22156507fae21512d46fbcbf7
Kirlian Pacibe <127189354+Kyverus@users.noreply.github.com>
2025-08-30 01:15:29 +0800

    Update credentials.js

--- src/middleware/credentials.js
+++ src/middleware/credentials.js
@@ -8,5 +8,10 @@ export function credentials(req, res, next) {
   }
 
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Origin, Authorization, Accept"
+  );
   next();
 }
