fb2e6a4654f9cc5e1701bbbf7a8721ff00af3486
Kirlian Pacibe <127189354+Kyverus@users.noreply.github.com>
2025-08-30 09:19:25 +0800

    Update credentials.js

--- src/middleware/credentials.js
+++ src/middleware/credentials.js
@@ -3,8 +3,12 @@ import { allowedOrigins } from "../config/allowedOrigins.js";
 export function credentials(req, res, next) {
   const origin = req.headers.origin;
 
-  if (!allowedOrigins.includes(origin)) {
+  if (origin && !allowedOrigins.includes(origin)) {
     return res.status(403).json({ message: "Not allowed by CORS" });
+  }
+
+  if (origin) {
+    res.header("Access-Control-Allow-Credentials", "true");
   }
 
   next();
